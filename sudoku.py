d80f4b4652336348b597936ad4a563184e251fb5
magfmur <natestutte@protonmail.com>
2020-03-17 17:38:04 -0400

    Changed __str__ to be more visually appealing to a real sudoku board

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,21 @@ class Sudoku:
         self.puzzle = np.full((9, 9), ' ', dtype=str)
 
     def __str__(self):
-        return str(self.puzzle)
+        horizontal_line = "+---+---+---+---+---+---+---+---+---+"
+        horizontal_lineless = "+ - + - + - + - + - + - + - + - + - +"
+        sudoku_board = ""
+
+        for a in range(9):
+            if a % 3 == 0:
+                sudoku_board += horizontal_line + "\n"
+            else:
+                sudoku_board += horizontal_lineless + "\n"
+            for b in range(9):
+                sudoku_board += f"| {self.puzzle[a, b]} "
+            sudoku_board += "|\n"
+        sudoku_board += horizontal_line
+
+        return sudoku_board
 
     def get(self, coords):
         if self.puzzle[coords[1], coords[0]] == ' ':
